d0cef69bf542f9861cf4c4bd60ec2e40193a49fe
felipecataneo <felipecataneo@hotmail.com>
2022-06-24 17:19:36 -0300

    first commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC =10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3bd81927-3643-4c74-b6e8-e73055c23ea8';
+export const MODAL_CLOSE_SEC = 2.5;
